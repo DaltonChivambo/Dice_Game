97554466bfc22b667395dc2612b657ccef1bbbb3
dalton <daltonchivambo@gmail.com>
2022-10-18 10:00:06 +0200

    Set icon image

--- app/src/main/java/com/skycoop/dice_game/MainActivity.java
+++ app/src/main/java/com/skycoop/dice_game/MainActivity.java
@@ -3,12 +3,34 @@ package com.skycoop.dice_game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView dice;
+
+    private int[] number_dice = {
+            R.drawable.dice_one,
+            R.drawable.dice_two,
+            R.drawable.dice_three,
+            R.drawable.dice_four,
+            R.drawable.dice_five,
+            R.drawable.dice_six
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dice = findViewById(R.id.dice);
+
+        dice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
